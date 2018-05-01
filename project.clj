ee3c1d96ff025be432a5c15cb5628a76caa1b403
Connor McKelvey <connormckelvey@gmail.com>
2018-05-01 11:19:14 -0600

    Exclude this garbage

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.kafka/connect-api "1.0.0" :exclusions [org.slf4j/slf4j-log4j12]]
-                 [io.confluent/kafka-connect-s3 "4.0.0"]]
+                 [io.confluent/kafka-connect-s3 "4.0.0" :exclusions [org.apache.curator/curator-framework]]]
 
   :java-source-paths ["src/java/prism_kafka_connect"]
   :jar-name "prism-kafka-connect.jar"
