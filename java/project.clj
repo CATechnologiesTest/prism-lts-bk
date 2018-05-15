bde92dbd96f45937b16bdbd3f51e2d3eb6ae3510
Charlotte Taylor <charlotte.taylor7693@gmail.com>
2018-05-15 13:41:58 -0600

    partition by topic and metric date and clean up

---
+++ java/project.clj
@@ -0,0 +1,16 @@
+(defproject prism-kafka-connect "0.1.0"
+  :description "A service called prism-kafka-connect"
+  :url "https://github.com/RallySoftware/prism-kafka-connect"
+  :license "Copyright 2016 CA Technologies"
+  :repositories [["confluent" "http://packages.confluent.io/maven"]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.kafka/connect-api "0.11.0.1" :exclusions [org.slf4j/slf4j-log4j12]]
+                 [io.confluent/kafka-connect-s3 "3.3.1" :exclusions[io.confluent/kafka-connect-storage-hive]]]
+
+  :java-source-paths ["src/java/prism_kafka_connect"]
+
+  :jar-name "prism-kafka-connect.jar"
+  :uberjar-name "prism-kafka-connect-standalone.jar"
+
+  :main prism-kafka-connect.core)
---
+++ java/src/java/prism_kafka_connect/partitioner/HourlyPartitioner.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prism_kafka_connect.partitioner;
+
+import org.apache.kafka.common.config.ConfigException;
+import org.joda.time.DateTimeZone;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import io.confluent.connect.storage.partitioner.*;
+
+import io.confluent.connect.storage.common.StorageCommonConfig;
+
+public class HourlyPartitioner<T> extends TimeBasedPartitioner<T> {
+  @Override
+  public void configure(Map<String, Object> config) {
+    long partitionDurationMs = TimeUnit.HOURS.toMillis(1);
+    String delim = (String) config.get(StorageCommonConfig.DIRECTORY_DELIM_CONFIG);
+    String pathFormat =
+        "'year'=YYYY" + delim + "'month'=MM" + delim + "'day'=dd" + delim + "'hour'=HH";
+
+    String localeString = (String) config.get(PartitionerConfig.LOCALE_CONFIG);
+    if (localeString.equals("")) {
+      throw new ConfigException(PartitionerConfig.LOCALE_CONFIG,
+                                localeString, "Locale cannot be empty.");
+    }
+    String timeZoneString = (String) config.get(PartitionerConfig.TIMEZONE_CONFIG);
+    if (timeZoneString.equals("")) {
+      throw new ConfigException(PartitionerConfig.TIMEZONE_CONFIG,
+                                timeZoneString, "Timezone cannot be empty.");
+    }
+    Locale locale = new Locale(localeString);
+    DateTimeZone timeZone = DateTimeZone.forID(timeZoneString);
+    init(partitionDurationMs, pathFormat, locale, timeZone, config);
+  }
+}
