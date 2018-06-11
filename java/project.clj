f39b95a426764c709adb168635ecc2cf9198398c
Connor McKelvey <connormckelvey@gmail.com>
2018-06-11 09:12:40 -0600

    4.1.1

--- java/project.clj
+++ java/project.clj
@@ -5,9 +5,9 @@
   :repositories [["confluent" "http://packages.confluent.io/maven"]]
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.kafka/connect-api "1.0.1" :exclusions [org.slf4j/slf4j-log4j12
+                 [org.apache.kafka/connect-api "1.1.0" :exclusions [org.slf4j/slf4j-log4j12
                                                                     org.apache/xerces]]
-                 [io.confluent/kafka-connect-s3 "4.0.1" :exclusions [io.confluent/kafka-connect-storage-hive
+                 [io.confluent/kafka-connect-s3 "4.1.1" :exclusions [io.confluent/kafka-connect-storage-hive
                                                                      org.apache/xerces]]]
 
   :java-source-paths ["src/java/prism_kafka_connect"]
