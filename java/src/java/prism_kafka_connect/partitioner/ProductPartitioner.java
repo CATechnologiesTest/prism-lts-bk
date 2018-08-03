3687ffe92fa4fda25f4d77feb4d9ccd7411e4947
Shwetha Bharadwaj <bhash12@bamye01mac1243.ca.com>
2018-08-03 16:28:14 -0700

    ProductPartitioner edit with new fieldnames

--- java/src/java/prism_kafka_connect/partitioner/ProductPartitioner.java
+++ java/src/java/prism_kafka_connect/partitioner/ProductPartitioner.java
@@ -35,33 +35,75 @@ import io.confluent.connect.storage.common.SchemaGenerator;
 import io.confluent.connect.storage.common.StorageCommonConfig;
 import io.confluent.connect.storage.errors.PartitionException;
 
-
+//customerid/productid/productinstanceid/metric-date
 public class ProductPartitioner<T> extends DefaultPartitioner<T> {
   protected static final Logger log = LoggerFactory.getLogger(FieldPartitioner.class);
-  protected List<String> fieldNames;
+  //for saas and health
+  protected List<String> fieldNamesSites;//create get method
+    //for site based
+    protected List<String> fieldNamesSaas;//create get method
+    protected List<String> fieldNamesUserEvents;
 
   @SuppressWarnings("unchecked")
   @Override
   public void configure(Map<String, Object> config) {
-    fieldNames = (List<String>) config.get(PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    fieldNamesSites = (List<String>) config.get("partition.sites.field.name");
+    fieldNamesSaas = (List<String>) config.get("partition.saas.field.name");
+    fieldNamesUserEvents = (List<String>) config.get("partition.userevent.field.name");
     delim = (String) config.get(StorageCommonConfig.DIRECTORY_DELIM_CONFIG);
   }
+//method to check if field has anothername in the schema
+ /* public String checkForAlias(String fieldName, Struct value) {
+        if(fieldName.equals("customer_id")) {//could either be user_id or customer_id
+            if(value.get("customer_id")!=null)
+                return "customer_id";
+            else if(value.get("user_id")!=null)
+                return "user_id";
+            else if(value.get("user_oid")!=null)
+                return "user_oid";
+            else {
+                return null;
 
-    @Override
-    public String encodePartition(SinkRecord sinkRecord) {
+            }
+        }
+        if(fieldName.equals("product_instance_id")) {//must be instance_id
+            if(value.get("product_instance_id") != null)
+                return "product_instance_id";
+            else if(value.get("instance_id") != null)
+                return "instance_id";
+            else
+                return null;
+        }
+        else
+            return null;
+
+  }
+*/
+
+  //method to partition metric_date
+
+  @Override
+  public String encodePartition(SinkRecord sinkRecord) {
         Object value = sinkRecord.value();
+        List <String> schemaFieldNames;
         int count = 0;
         if (value instanceof Struct) {
             final Schema valueSchema = sinkRecord.valueSchema();
             final Struct struct = (Struct) value;
 
             StringBuilder builder = new StringBuilder();
-            for (String fieldName : fieldNames) {
+            schemaFieldNames = whichFieldNames(struct,valueSchema);
+            for (String fieldName : schemaFieldNames) {
                 if (builder.length() > 0) {
                     builder.append(this.delim);
                 }
 
                 Object partitionKey = struct.get(fieldName);
+                /*if(partitionKey == null) {
+                    fieldName = checkForAlias(fieldName,struct);
+                    partitionKey = struct.get(fieldName);
+
+                }*/
                 Type type = valueSchema.field(fieldName).schema().type();
                 switch (type) {
                     case INT8:
@@ -101,5 +143,15 @@ public class ProductPartitioner<T> extends DefaultPartitioner<T> {
         }
     }
 
+    public List<String> whichFieldNames(Struct value, Schema valueSchema) {
+        if(valueSchema.name().equals("saas-usage-metrics")||valueSchema.name().equals("health-metrics")){
+            return fieldNamesSaas;
+        }
+        else if(valueSchema.name().equals("ac-user-event")) {
+            return fieldNamesUserEvents;
+        }
+        else
+            return fieldNamesSites;
 
+    }
 }
